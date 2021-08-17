34c97d7a6ed80ad70e3d93149cfc3644c20f14f8
1binibin <namwonbin@gmail.com>
2021-08-17 13:18:06 +0900

    검색 완료 -페이징 이랑 뭐한다고함

--- js/02.search.js
+++ js/02.search.js
@@ -146,7 +146,22 @@ function setBookLists(r) {
 }
 
 function setCafeLists(r) {
-    console.log(r);
+    $('.lists').empty().attr('class', 'lists cafe');
+    var html = '';
+    r.forEach(function(v, i) {
+        html ='<li class="list">'
+        html +='<a href="'+v.url+'" class="thumbs" target="_blank">'
+        html +='<img src="'+v.thumbnail+'" alt="'+v.title+'" class="w100">'
+        html +='</a>'
+        html +='<div class="contents">'
+        html +='<a class="title" href="'+v.url+'" target="_blank">'+v.title+'</a>'
+        html +='<p class="content">'+v.contents+'</p>'
+        html +='<a class="name" href="'+v.url+'" target="_blank">'+v.cafename+'</a> | <a href="" class="link">'+v.url+'</a>'
+        html +='<div class="dt">'+moment(v.datatime).format('YYYY-MM-DD HH:mm:ss')+'</div>'
+        html +='</div>'
+        html +='</li>'
+        $('.lists').append(html);
+    });;
 }
 
 /*************** event callback *****************/
